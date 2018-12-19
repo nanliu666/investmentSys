4dea368681783d04d3a1a2b3558b37dedacb2457
NaNSix <243595837@qq.com>
2018-12-19 12:03:07 +0800

    使用vux组件的商机增加组件

--- src/page/business/businessAdd.vue
+++ src/page/business/businessAdd.vue
@@ -6,56 +6,27 @@
         <div class="danyuan">当前预定单元</div>
         <div class="qi">星月湾·东街二期 &nbsp; 403</div>
       </div>
-      <form action>
-        <section class="main">
-          <div class="cientInfo">客户信息</div>
-          <section class="formInfo">
-            <div>
-              <label for>
-                客户姓名
-                <span class="start">*</span>
-              </label>
-              <input type="text" placeholder="请选择客户" required>
-              <i class="iconfont icon-youjiantou"></i>
-            </div>
-            <div>
-              <label for>手机号码</label>
-              <input type="number" name id>
-            </div>
-          </section>
-        </section>
-        <section class="main">
-          <div class="cientInfo">商机信息</div>
-          <section class="formInfo">
-            <div>
-              <label for>商机来源</label>
-              <input type="text" placeholder="请选择客户" required>
-              <i class="iconfont icon-youjiantou"></i>
-            </div>
-            <div>
-              <label for>总面积</label>
-              <input type="number" name id>
-            </div>
-            <div>
-              <label for>预计成交金额</label>
-              <input type="number" name id>
-            </div>
-            <div>
-              <label for>紧急程度</label>
-              <!-- <input type="number" name id> -->
-              <popup></popup>
-              <i class="iconfont icon-youjiantou"></i>
-            </div>
-            <div>
-              <label for>备注</label>
-              <input type="number" name id>
-            </div>
-          </section>
-        </section>
-        <section class="button">
-          <button class="submit">保存</button>
-        </section>
-      </form>
+      <group class="group">
+        <div class="cientInfo">客户信息</div>
+
+        <cell title="客户姓名" link='/clientList'>
+          <span slot="title" class="phone">
+            <span style="vertical-align:middle;">客户姓名</span>
+            <badge text="*" class="badge"></badge>
+            <input type="text" required value="张三">
+          </span>
+        </cell>
+        <x-input title="手机号码" placeholder="请输入手机号" v-model="phoneValue"></x-input>
+        <div class="cientInfo">商机信息</div>
+        <popup-picker title="商机来源" :data="sourceList" v-model="sourceValue"></popup-picker>
+        <cell title="总面积" value="500.25 m²"></cell>
+        <cell title="预计成交金额" value="¥453.5万"></cell>
+        <popup-picker title="紧急程度" :data="urgencyList" v-model="urgencyValue"></popup-picker>
+        <x-textarea title="备注" placeholder="请填写备注" :show-counter="false" :rows="3"></x-textarea>
+      </group>
+      <section class="button">
+        <x-button class="submit" @click.native="submit">保存</x-button>
+      </section>
     </section>
   </div>
 </template>
@@ -63,33 +34,88 @@
 
 <script>
 import headerTop from "@/components/headerTOP";
-import popup from "@/components/popup";
+import {
+  Datetime,
+  Group,
+  Cell,
+  Picker,
+  XButton,
+  Divider,
+  XSwitch,
+  XTextarea,
+  XInput,
+  Badge
+} from "vux";
+
+// api
 import {
   GetBizOpportunity,
   GetBizOpportunityDetail,
+  GetUnitByBlock,
   GetBizopprtunityDropdown
 } from "@/axios/api";
+
 export default {
   name: "reserve",
   data() {
-    return {};
+    return {
+      urgencyList: [["不重要", "一般", "重要", "非常重要"]],
+      sourceList: [
+        [
+          "电视电台",
+          "广告杂志",
+          "户外广告",
+          "朋友介绍",
+          "网上展销",
+          "宣传单张",
+          "邮件短信"
+        ]
+      ],
+      value1: ["iPhone"],
+      nameValue: "张三",
+      urgencyValue: ["一般"],
+      phoneValue: "136445678786",
+      sourceValue: ["电视电台"],
+      unitInfoName: '',
+    };
   },
   created() {
-    this.onLoad()
+    this.onLoad();
   },
   components: {
     headerTop,
-    popup
+    Group,
+    Picker,
+    XButton,
+    Divider,
+    Cell,
+    XSwitch,
+    Badge,
+    XInput,
+    XTextarea,
+    Datetime
   },
   methods: {
     onLoad() {
-      var data = {
-        Bizopportunity: {},
-        Prospectid: 0
-      };
-      GetBizopprtunityDropdown(data).then(res => {
-        console.log(res);
-      });
+      // var data = {
+      //   Bizopportunity: {},
+      //   Prospectid: 0
+      // };
+      // 模拟楼栋信息
+      const uBdata = {
+        Blockid: 0,
+        Statucode: 'UnitAvailable ', //可租
+        Floorno: '1',
+        Startdate: '' ,
+        Enddate: '' ,
+      }
+
+      GetUnitByBlock(uBdata).then(res => {
+        console.log(res)
+      })
+    },
+    submit() {
+      this.$router.push('/businessList')
     }
   }
 };
@@ -103,6 +129,7 @@ export default {
     .reseveTitle {
       height: 160px;
       padding: 32px 40px;
+      margin-bottom: -44px; /*no*/
       background: linear-gradient(
         to left,
         rgba(56, 153, 255, 1),
@@ -119,56 +146,19 @@ export default {
         font-family: $familyMedium;
       }
     }
-    .main {
-      .cientInfo {
-        @include sc(28px, #1e1e1e);
-        font-family: $familyR;
-        padding: 16px 40px;
-      }
-      .formInfo {
-        padding: 0 40px;
-        background-color: #fff;
-        div {
-          position: relative;
-          padding: 20px 0;
-          border-bottom: 1px solid #ececec; /*no*/
-          label {
-            @include sc(30px, #888);
-          }
-          input {
-            position: absolute;
-            left: 240px;
-          }
-        }
-        div:last-child {
-          border-bottom: 0px solid #ececec; /*no*/
-        }
-        .start {
-          color: red;
-        }
-        .icon-youjiantou {
-          position: absolute;
-          right: 8px;
-          @include wh(8px, 16px);
-          color: rgba(219, 219, 219, 1);
-        }
-      }
-      .upload {
-        div {
-          @include fj(space-between);
-          input {
-            width: 60%;
-          }
-          button {
-            @include wh(114px, 50px);
-            @include borderRadius(25px);
-            @include borderStyle(rgba(72, 121, 230, 1));
-            @include sc(28px, rgba(72, 121, 230, 1));
-            padding: 6px 18px;
-            background-color: $fc;
-          }
-        }
-      }
+    .cientInfo {
+      // @include sc(28px, #1e1e1e);
+      // font-family: $familyR;
+      padding: 16px 40px;
+      background-color: $bc;
+    }
+    .phone {
+      background-color: #fff;
+      color: #000;
+    }
+    .badge {
+      background-color: #fff;
+      color: red;
     }
     .button {
       @include fj(space-around);
@@ -191,5 +181,6 @@ export default {
   }
 }
 </style>
+
 
 
