323b348829277b18b78a43ec18d71f8676392e6f
NaNSix <243595837@qq.com>
2018-12-21 18:10:36 +0800

    修改vux头部组件公共样式

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,15 @@ export default {
 </script>
 <style lang="scss">
 @import '@/assets/sass/common.scss';
+@import '@/assets/sass/mixin.scss';
 #app {
   height: 100%;
+  .header {
+    background-color: #fff;
+    box-shadow: 0 4px 14px 0 rgba(126, 158, 230, 0.15);
+    h1 {
+      @include sc(34px, rgba(3, 3, 3, 1))
+    }
+  }
 }
 </style>
