0e2f68437c063f6b0110b82d485ee86a8e4a6466
NaNSix <243595837@qq.com>
2018-12-26 16:27:16 +0800

    新增数字过滤器

--- src/assets/js/filters.js
+++ src/assets/js/filters.js
@@ -4,14 +4,52 @@
  */
 // moment 开启cnd加速，不需要在这里import
 //格式化日期YYY-MM-DD
-let dataFrm = date => {
-  return moment(date).format("YYYY-MM-DD")
+let dataFrm = (date, format) => {
+  return moment(date).format(format)
 }
 //截取第一个字符串
 let strSubstring = str => {
-  return str.substring(0,1)
+  return str.substring(0, 1)
+}
+
+//格式化数字 逢千加逗号
+let formatNumber = (number, decimals = 2, dec_point = '.', thousands_sep = ',', roundtag) => {
+  /*
+   * 参数说明：
+   * number：要格式化的数字
+   * decimals：保留几位小数
+   * dec_point：小数点符号
+   * thousands_sep：千分位符号
+   * roundtag:舍入参数，默认 "ceil" 向上取,"floor"向下取,"round" 四舍五入
+   * */
+  number = (number + '').replace(/[^0-9+-Ee.]/g, '');
+  roundtag = roundtag || "ceil"; //"ceil","floor","round"
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function (n, prec) {
+
+      var k = Math.pow(10, prec);
+      console.log();
+
+      return '' + parseFloat(Math[roundtag](parseFloat((n * k).toFixed(prec * 2))).toFixed(prec * 2)) / k;
+    };
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  var re = /(-?\d+)(\d{3})/;
+  while (re.test(s[0])) {
+    s[0] = s[0].replace(re, "$1" + sep + "$2");
+  }
+
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
 }
 export {
   dataFrm,
   strSubstring,
+  formatNumber,
 }
