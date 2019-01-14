4689c0613966f9154c19e3345f9a2109839d5f9b
NaNSix <243595837@qq.com>
2019-01-14 18:27:49 +0800

    信息列表

--- src/page/home/homeNew.vue
+++ src/page/home/homeNew.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>1</div>
+  <div>
+    <x-header class="header" :left-options="{backText: ''}">信息列表</x-header>
+    <section class="newList"></section>
+  </div>
 </template>
 
 <script>
+import { XHeader } from "vux";
+import { getAgencyAndNewest } from "@/axios/api";
 export default {
-  name: "homeNew"
+  name: "homeNew",
+  components: { XHeader },
+  created() {
+    this.onLoad();
+  },
+  methods: {
+    onLoad() {
+      const data = {
+        LoginName: "qw"
+      };
+      getAgencyAndNewest(data).then(res => {
+        console.log(res);
+      });
+    }
+  }
 };
 </script>
 
