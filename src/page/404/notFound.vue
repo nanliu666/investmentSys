45c033f7a4d168ada7250ca117ae7113013c5c11
NaNSix <243595837@qq.com>
2019-01-23 09:58:16 +0800

    新增404

---
+++ src/page/404/notFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="notFound">
+    <div>
+      <img src="../../assets/images/分组.png" alt>
+    </div>
+    <div>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-shuaxinzhongjieban"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "notFound"
+};
+</script>
+<style lang="scss" scoped>
+@import "src/assets/sass/mixin";
+.notFound {
+}
+</style>
